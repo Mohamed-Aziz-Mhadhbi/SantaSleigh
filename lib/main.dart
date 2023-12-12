710e828445432bdc0b9d08bc664b5db2bd154cd3
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-12 10:38:47 +0100

    adding parallaxComponent for the backgound image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,5 +8,13 @@ void main() {
 }
 
 class SantaGame extends FlameGame {
-  
+  @override
+  void onLoad() async {
+    super.onLoad();
+    ParallaxComponent mountinBackgound = await loadParallaxComponent(
+      [ParallaxImageData('background.png')],
+      baseVelocity: Vector2(10, 0),
+      velocityMultiplierDelta: Vector2(1.6, 1.0),
+    );
+  }
 }
