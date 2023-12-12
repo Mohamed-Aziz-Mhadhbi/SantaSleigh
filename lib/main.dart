48b8d6895f3e067467896d82f29daf7d03fa73f0
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2023-12-12 15:49:53 +0100

    update santa movment

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,61 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flame/parallax.dart';
+import 'package:flame/src/gestures/events.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(GameWidget(game: SantaGame()));
 }
 
-class SantaGame extends FlameGame {
+class SantaGame extends FlameGame with TapDetector {
+  late SpriteAnimationComponent santa;
+  Vector2 gravity = Vector2(0, 30);
+
   @override
   void onLoad() async {
     super.onLoad();
     ParallaxComponent mountinBackgound = await loadParallaxComponent(
       [
-        ParallaxImageData('background.png'),
+        ParallaxImageData('sky.png'),
+        ParallaxImageData('clouds_bg.png'),
+        ParallaxImageData('glacial_mountains.png'),
         ParallaxImageData('clouds_mg_1.png'),
-        ParallaxImageData('clouds_mg_2.png'),
-        ParallaxImageData('clouds_mg_3.png'),
         ParallaxImageData('cloud_lonely.png'),
       ],
-      baseVelocity: Vector2(40, 0),
+      baseVelocity: Vector2(50, 0),
       velocityMultiplierDelta: Vector2(1.6, 1.0),
     );
     add(mountinBackgound);
+    final santaAnimation = await loadSpriteAnimation(
+      "santa.png",
+      SpriteAnimationData.sequenced(
+        amount: 24,
+        amountPerRow: 4,
+        stepTime: 0.1,
+        textureSize: Vector2(30, 20),
+      ),
+    );
+    santa = SpriteAnimationComponent(
+      animation: santaAnimation,
+      position: size / 2,
+      anchor: Anchor.center,
+      size: Vector2(size.y * 100 / 40, size.y) * .1,
+    );
+    add(santa);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    gravity.y += .4;
+    santa.position += gravity * dt;
+  }
+
+  @override
+  void onTapUp(TapUpInfo info) {
+    gravity.y -= .2;
+    super.onTapUp(info);
   }
 }
